a5dfff91625ab05928c27fdf99a459f1730284df
Abel Pelser <abelpelser@gmail.com>
2021-12-01 06:07:12 +0100

    Day1 - ugly

--- puzzles/Day1/day1.py
+++ puzzles/Day1/day1.py
@@ -2,11 +2,30 @@ from util import read_input_as_numbers
 
 
 def part1():
-    return 0
+    numbers = read_input_as_numbers()
+    prev = None
+    c = 0
+    for n in numbers:
+        if prev is not None and n > prev:
+            c += 1
+        prev = n
+    return c
 
 
 def part2():
-    return 0
+    numbers = read_input_as_numbers()
+    prev = None
+    prevb = None
+    c = 0
+    last_s = None
+    for n in numbers:
+        if prev is not None and prevb is not None and last_s is not None and sum((n, prev, prevb)) > last_s:
+            c += 1
+        if prev is not None and prevb is not None:
+            last_s = sum((n, prev, prevb))
+        prevb = prev
+        prev = n
+    return c
 
 
 if __name__ == '__main__':
