f7e2a37945ffd9dd3e51bf611a55193964023651
Abel Pelser <abelpelser@gmail.com>
2021-12-02 06:04:44 +0100

    Day2 - ugly

--- puzzles/Day2/day2.py
+++ puzzles/Day2/day2.py
@@ -1,12 +1,36 @@
-from util import read_input_as_numbers
+from util import read_input_as_lines, read_input_as_numbers
 
 
 def part1():
-    return 0
+    lines = read_input_as_lines()
+    depth = 0
+    pos = 0
+    for l in lines:
+        if l.startswith('forward '):
+            pos += int(l[8:])
+        elif l.startswith('down '):
+            depth += int(l[5:])
+        elif l.startswith('up '):
+            depth -= int(l[3:])
+    return pos * depth
+
 
 
 def part2():
-    return 0
+    lines = read_input_as_lines()
+    aim = 0
+    pos = 0
+    depth = 0
+    for l in lines:
+        if l.startswith('forward '):
+            val = int(l[8:])
+            pos += val
+            depth += aim * val
+        elif l.startswith('down '):
+            aim += int(l[5:])
+        elif l.startswith('up '):
+            aim -= int(l[3:])
+    return pos * depth
 
 
 if __name__ == '__main__':
