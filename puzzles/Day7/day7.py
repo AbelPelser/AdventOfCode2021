e339e57e495286a6e7b414f30820902db6cb3008
Abel Pelser <abelpelser@gmail.com>
2021-12-07 06:14:06 +0100

    Day 7 - ugly

--- puzzles/Day7/day7.py
+++ puzzles/Day7/day7.py
@@ -1,12 +1,39 @@
-from util import read_input_as_numbers
+from util import *
+
+
+def cost_part2(a, b):
+    c = 0
+    for i in range(1, abs(a - b) + 1):
+        c += i
+    return c
 
 
 def part1():
-    return 0
+    lines = read_input_as_lines()
+    numbers = list(map(int, lines[0].split(',')))
+
+    best_cost = None
+    for n in range(min(numbers), max(numbers)):
+        cost = 0
+        for number in numbers:
+            cost += abs(n - number)
+        if best_cost is None or cost < best_cost:
+            best_cost = cost
+    return best_cost
 
 
 def part2():
-    return 0
+    lines = read_input_as_lines()
+    numbers = list(map(int, lines[0].split(',')))
+
+    best_cost = None
+    for n in range(min(numbers), max(numbers)):
+        cost = 0
+        for number in numbers:
+            cost += cost_part2(n, number)
+        if best_cost is None or cost < best_cost:
+            best_cost = cost
+    return best_cost
 
 
 if __name__ == '__main__':
